30c2eb8c59ee095ee493c1be9a357d3a54566de2
MARCTELLY <marctelly@hotmail.fr>
2024-06-29 17:48:45 +0200

    feat: add account

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,15 +1,22 @@
+use std::collections::HashMap;
 use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+pub struct BaseConfig {
+    #[serde(rename = "api-config")]
+    pub api_config: ApiConfig
+}
 
 #[derive(Debug, Deserialize)]
 pub struct ApiConfig {
+    pub base_url : String,
     pub api_key : String,
     pub secret : String
 }
 
 /// Build the configuration of the api
-pub fn build_config() -> Result<ApiConfig, ConfigError> {
+pub fn build_config() -> Result<BaseConfig, ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     tracing::event!(tracing::Level::INFO,"{}", base_path.to_str().unwrap());
 
@@ -20,7 +27,7 @@ pub fn build_config() -> Result<ApiConfig, ConfigError> {
         ))
         .build()?;
 
-    let conf = settings.try_deserialize::<ApiConfig>()?;
+    let conf = settings.try_deserialize::<BaseConfig>()?;
     Ok(conf)
 }
 
@@ -31,14 +38,14 @@ mod tests {
 
     #[test]
     fn create_config(){
-        let config = ApiConfig{ api_key: "api_key".to_string(), secret: "secret_key".to_string() };
+        let config = ApiConfig{ base_url: "".to_string(), api_key: "api_key".to_string(), secret: "secret_key".to_string() };
         assert_eq!(config.api_key, "api_key")
     }
 
     #[test]
     fn read_config_from_yaml() {
         let config = build_config();
-        assert_eq!(config.expect("error in config building").api_key, "api_key")
+        assert_eq!(config.expect("error in config building").api_config.api_key, "api_key")
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,50 @@
+use apca::{ApiInfo, Client, api::v2::{account}, RequestError};
+use apca::api::v2::account::{Account, GetError};
+use tokio::task::unconstrained;
+use tracing::instrument::WithSubscriber;
+use crate::configuration::BaseConfig;
+
 mod trade;
 mod configuration;
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
+    // init tracing
+    tracing_subscriber::fmt::init();
+
+    tracing::info!("App start");
     // connection to
-    todo!()
+    // configuration of api
+    let config = configuration::build_config().expect("cannot load configuration");
+    let api_config = config.api_config;
+    let api_config = ApiInfo::from_parts(
+        api_config.base_url,
+        api_config.api_key,
+        api_config.secret
+    ).unwrap();
+
+    // alpaca client
+    let client = Client::new(api_config);
+    let account = get_account(client).await.unwrap();
+
+    tracing::event!(
+        name: "account_info",
+        tracing::Level::INFO,
+        r#"
+        account_id : {:?}
+        "#,
+        account.id
+    )
+
+}
+
+async fn get_account(client: Client) -> Result<Account, ()> {
+    tracing::info!("app_events: get account information");
+    let account = client
+        .issue::<account::Get>(&())
+        .await
+        .expect("error to get account");
+
+    Ok(account)
 }
